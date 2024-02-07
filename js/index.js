1d66c607b4c5a19e2d442e44d40aaa2c476b2884
austin-jarrett-dev <austin.jarrett.dev+github@gmail.com>
2024-02-06 22:05:43 -0500

    fix responsive nav impl

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navList = document.querySelector(".nav__list");
+  const hamburger = document.querySelector(".hamburger");
+  const navList = document.querySelector(".nav__list");
 
-    hamburger.addEventListener("click", function () {
-      navList.classList.toggle("is-active");
-    });
+  hamburger.addEventListener("click", function () {
+    navList.classList.toggle("is-active");
   });
+
+  document.addEventListener("click", function (event) {
+    if (
+      event.target.closest(".nav__list") ||
+      event.target.closest(".hamburger")
+    ) {
+      return;
+    }
+    navList.classList.remove("is-active");
+  });
+});
